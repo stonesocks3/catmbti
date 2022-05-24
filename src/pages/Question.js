933abf4db7c3abd8a627315169942cb1297f5e9e
MJ JI <stonesocks3@gmail.com>
2022-05-25 04:17:28 +0900

    add data

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { QuestionData } from "../assets/data/Questiondata";
 
 function Question() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Question.js입니다!
-      </p>
-      <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
-    </div>
+    <section>
+      <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <p class="flex justify-center text-3xl text-gray-700 font-bold mb-5">
+          {QuestionData[0].title}
+        </p>
+        {/*  <p className="text-gray-500 text-lg">
+          React and Tailwind CSS in action
+        </p> */}
+      </div>
+      <buttonGroup class="flex flex-row justify-center">
+        <button
+          class="w-2/5 h-[200pt] px-6 rounded-lg bg-indigo-600 text-white text-2xl font-mono mr-5 "
+          type="button"
+        >
+          {QuestionData[0].answera}
+        </button>
+        <button
+          class="w-2/5 h-[200pt] px-6 rounded-lg bg-indigo-600 text-white text-2xl font-mono ml-5"
+          type="button"
+        >
+          {QuestionData[0].answerb}
+        </button>
+      </buttonGroup>
+    </section>
   );
 }
 export default Question;
