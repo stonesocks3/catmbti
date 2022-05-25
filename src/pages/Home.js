c5bc59ee5c1434878d91dcc32b1504e4bee99e97
MJ JI <stonesocks3@gmail.com>
2022-05-25 23:22:19 +0900

    add ProgressBar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,29 +9,29 @@ const Home = () => {
   };
   return (
     <>
-      <header class="container mx-auto bg-sky-500 rounded-xl shadow border p-8 m-10">
-        <p class="flex justify-center text-[40pt] text-gray-700 font-bold mb-4 font-mono">
+      <header className="container mx-auto bg-sky-500 rounded-xl shadow border p-8 m-10">
+        <p className="flex justify-center text-[40pt] text-gray-700 font-bold mb-4 font-mono">
           예비집사 판별기
         </p>
       </header>
-      <section class="flex justify-center">
-        <div>
-          <p class=" text-pink-700 font-bold text-[30pt] mt-[40px] font-mono ">
+      <section className="flex justify-center">
+        <div className="text-center">
+          <p className=" text-pink-700 font-bold text-[30pt] mt-[40px] font-mono ">
             나에게 맞는 주인님은??
           </p>
           <img
-            class="w-100 h-100 rounded-full mt-2.5 align-center"
+            className="w-100 h-100 rounded-full mt-10 ml-20 "
             src={photo338}
             alt=""
             width="384"
             height="512"
           />
 
-          <p class=" mt-5 text-[20pt] text-gray-700 font-bold mb-5 font-mono">
+          <p className=" mt-5 text-[20pt] text-gray-700  mb-5 font-mono">
             MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기
           </p>
           <button
-            class="h-10 px-6 rounded-lg bg-indigo-600 text-white text-sm font-mono"
+            className="h-10 px-6 rounded-lg bg-indigo-600 text-white text-sm font-mono "
             type="button"
             onClick={handleClickButton}
           >
--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,31 +1,61 @@
 import React from "react";
 import { QuestionData } from "../assets/data/Questiondata";
 
+
+
+
 function Question() {
+  const [questionNo, setQuestionNo] = React.useState(0);
+  const [totalScore,setTotalScore] = React.useState();
+
+  const progressPercentage = (questionNo / QuestionData.length * 100);
+
+  const handleClickButtonA = (e) => {
+    if (0 < questionNo < QuestionData.length) setQuestionNo(questionNo + 1);
+    else {
+      setQuestionNo(12);
+    }
+  };
+  const handleClickButtonB = (e) => {
+    if (0 < questionNo < QuestionData.length + 1) setQuestionNo(questionNo - 1);
+    else {
+      setQuestionNo(1);
+    }
+  };
+
   return (
     <section>
-      <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-        <p class="flex justify-center text-3xl text-gray-700 font-bold mb-5">
-          {QuestionData[0].title}
-        </p>
-        {/*  <p className="text-gray-500 text-lg">
-          React and Tailwind CSS in action
-        </p> */}
+      <div>
+    {/* ProgressBar */}
+        <div className="w-full bg-gray-300  h-3.5 dark:bg-gray-700">
+          <div  style={{ width: `${progressPercentage}%`}}
+            className={`${
+              progressPercentage < 50 ? 'bg-pink-600' : 'bg-sky-500'} h-3.5 rounded-full` }
+          ></div>
+        </div>
+
+        <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+          <p className="flex justify-center text-3xl text-gray-700 font-bold mb-5">
+            {QuestionData[questionNo].title}
+          </p>
+        </div>
+        <article className="flex flex-row justify-center font-mono text-white text-2xl h-[200pt]">
+          <button
+            className="w-2/5 px-6 rounded-lg bg-indigo-600 mr-5"
+            type="button"
+            onClick={() => handleClickButtonA()}
+          >
+            {QuestionData[questionNo].answera}
+          </button>
+          <button
+            className="w-2/5  px-6 rounded-lg bg-indigo-600  ml-5"
+            type="button"
+            onClick={() => handleClickButtonB()}
+          >
+            {QuestionData[questionNo].answerb}
+          </button>
+        </article>
       </div>
-      <buttonGroup class="flex flex-row justify-center">
-        <button
-          class="w-2/5 h-[200pt] px-6 rounded-lg bg-indigo-600 text-white text-2xl font-mono mr-5 "
-          type="button"
-        >
-          {QuestionData[0].answera}
-        </button>
-        <button
-          class="w-2/5 h-[200pt] px-6 rounded-lg bg-indigo-600 text-white text-2xl font-mono ml-5"
-          type="button"
-        >
-          {QuestionData[0].answerb}
-        </button>
-      </buttonGroup>
     </section>
   );
 }
